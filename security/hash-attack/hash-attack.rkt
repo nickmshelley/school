bdce023cb674dc5bc91204d0cea321bf75ea3d7d
Nick Shelley <nickmshelley@gmail.com>
2011-09-20 20:47:51 -0600

    can generate all combinations of an arbitrary number of letters

--- security/hash-attack/hash-attack.rkt
+++ security/hash-attack/hash-attack.rkt
@@ -1,6 +1,44 @@
 #lang racket
-(require openssl/sha1)
-
-(define alphabet "abcdefghijklmnopqrstuvwxyz")
+(require openssl/sha1
+         racket/generator
+         test-engine/racket-tests)
 
 (sha1 (open-input-string "hi"))
+
+(define (next-word current-word)
+  (if (string=? current-word (make-string (string-length current-word) #\z))
+      #f
+      (list->string
+       (let loop ([old-word (reverse (string->list current-word))]
+                  [new-word empty]
+                  [changed-non-z #f])
+         (if (empty? old-word)
+             new-word
+             (if changed-non-z
+                 (loop (rest old-word)
+                       (cons (first old-word) new-word)
+                       changed-non-z)
+                 (if (char=? (first old-word) #\z)
+                     (loop (rest old-word)
+                           (cons #\a new-word)
+                           changed-non-z)
+                     (loop (rest old-word)
+                           (cons (integer->char (add1 (char->integer (first old-word)))) new-word)
+                           #t))))))))
+(check-expect (next-word "a") "b")
+(check-expect (next-word "at") "au")
+(check-expect (next-word "cz") "da")
+(check-expect (next-word "afzzz") "agaaa")
+(check-expect (next-word "zzz") #f)
+
+(define g (generator (size)
+            (let loop ([word (make-string size #\a)])
+              (yield word)
+              (loop (next-word word)))))
+
+(for ([i (in-producer g #f 2)])
+  (printf "~a~n" i))
+
+(test)
+
+
