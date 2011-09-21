7b5b271234a5bdf2e0025f92030639f6d3dad8e3
Nick Shelley <nickmshelley@gmail.com>
2011-09-20 23:31:43 -0600

    finished

--- security/hash-attack/hash-attack.rkt
+++ security/hash-attack/hash-attack.rkt
@@ -43,7 +43,30 @@
     (when (not (string=? result str)) (error 'collision-found "~a and ~a collided; hashes were:~n~a~n~a~nAttack took ~a steps"
                                              result str (sha1 (open-input-string result)) (sha1 (open-input-string str)) (length (hash-keys results))))))
 
-(collision-attack 5)
+(define (pre-image-attack character digits)
+  (define image (make-string digits character))
+  (define steps 0)
+  (for* ([size (in-naturals 1)]
+         [str (in-producer (make-generator size) #f)])
+    (define result (substring (sha1 (open-input-string str)) 0 digits))
+    (set! steps (add1 steps))
+    (when (string=? result image) (raise steps))))
+
+(define (pre-image-loop digits)
+  (define steps-sum 0)
+  (define seq "0123456789abcdef")
+  (for ([c seq])
+    (with-handlers 
+        ([number? 
+          (lambda (v) 
+            (set! steps-sum (+ v steps-sum))
+            (printf "steps: ~a~n" v))])
+      (pre-image-attack c digits)))
+  (printf "total: ~a~naverage: ~a~n" steps-sum (/ steps-sum 16.)))
+
+;(collision-attack 5)
+
+(pre-image-loop 4)
 
 ;(test)
 
