a0a9862cf9ccc97f65eaaee212403a35e951dc2d
Nick Shelley <nickmshelley@gmail.com>
2011-10-03 14:10:32 -0600

    Different starting point

--- graphics/water-sim/water-sim.rkt
+++ graphics/water-sim/water-sim.rkt
@@ -1,58 +1,56 @@
 #lang racket/gui
-
-(require (lib "gl.ss" "sgl")
-         (lib "gl-vectors.ss" "sgl"))
-
+(require sgl
+         sgl/gl-vectors)
 
 (define (resize w h)
-  (glViewport 0 0 w h)
+  (gl-viewport 0 0 w h)
   #t)
 
-(define x 0.0)
-(define fun +)
+(define (init-opengl)
+  (gl-clear-color 0 0 0 1)
+  (gl-color 1 1 1)
+  (gl-ortho -50 50 -50 50 -1 1)
+  (gl-shade-model 'flat))
+
+(define spin 0.0)
 
 (define (draw-opengl)
-  (glClearColor 0.0 0.0 0.0 0.0)
-  (glClear GL_COLOR_BUFFER_BIT)
-  (glColor3d 1.0 1.0 1.0)
-
-  (glMatrixMode GL_PROJECTION)
-  (glLoadIdentity)
-  (glOrtho 0.0 1.0 0.0 1.0 -1.0 1.0)
-  (glMatrixMode GL_MODELVIEW)
-  (glLoadIdentity)
-
-  (glBegin GL_QUADS)
-  (glVertex3d x 0.25 0.0)
-  (glVertex3d 0.75 0.25 0.0)
-  (glVertex3d 0.75 0.75 0.0)
-  (glVertex3d 0.25 0.75 0.0)
-  (glEnd)
-  (when (> x .5) (set! fun -))
-  (when (< x .1) (set! fun +))
-  (set! x (fun x 0.005)))
-
+  (define new-spin (+ spin 2.0))
+  (set! spin (if (> new-spin 360) (- new-spin 360) new-spin))
+  
+  (gl-clear 'color-buffer-bit 'depth-buffer-bit)
+  (gl-push-matrix)
+  (gl-rotate spin 0 0 -1)
+  (gl-rect -25 -25 25 25)
+  (gl-pop-matrix)
+  (gl-end))
 
 (define my-canvas%
   (class* canvas% ()
     (inherit refresh with-gl-context swap-gl-buffers)
-
-   (define/override (on-paint)
+    
+    (define/public (gl-init)
       (with-gl-context
-        (lambda ()
-          (draw-opengl)
-          (swap-gl-buffers)
-          (queue-callback (lambda x (send this refresh)) #f))))
-
+       (lambda ()
+         (init-opengl))))
+    
+    (define/override (on-paint)
+      (with-gl-context
+       (lambda ()
+         (draw-opengl)
+         (swap-gl-buffers)
+         (queue-callback (lambda x (send this refresh)) #f))))
+    
     (define/override (on-size width height)
       (with-gl-context
-        (lambda ()
-          (resize width height))))
-
+       (lambda ()
+         (resize width height))))
+    
     (super-instantiate () (style '(gl)))))
 
-(define win (new frame% (label "OpenGl Test") (min-width 200) (min-height
-200)))
+(define win (new frame% (label "OpenGl Test") (min-width 400) (min-height
+                                                               400)))
 (define gl  (new my-canvas% (parent win)))
 
+(send gl gl-init)
 (send win show #t)
