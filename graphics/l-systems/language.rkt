2b9a682cb24d42db60b5f04c66230e6dee9641f4
Nick Shelley <nickmshelley@gmail.com>
2011-11-18 12:14:36 -0700

    nonworking attempt to quote brackets

--- graphics/l-systems/language.rkt
+++ graphics/l-systems/language.rkt
@@ -18,31 +18,39 @@
     ...
     [x (list (list 1 (list x)))]))
 
-(define-syntax (l-system stx)
-  (syntax-parse stx
-                [(l-system 
-                  (~datum beta) (~datum ->) beta*:number
-                  (~datum start-angle) (~datum ->) start-angle*:number 
-                  (~datum length) (~datum ->) length*:number
-                  (~datum generations) (~datum ->) generations*:number
-                  (~datum axiom) (~datum ->) (~seq axiom*:id (~peek-not (~datum ->))) ...
-                  (~seq lhs (~datum ->) (~seq prob:number (~datum =>) 
-                                              (~seq prod:id (~peek-not (~datum ->)))
-                                              ...)
-                        ...)
-                  ...)
-                 #'(render (turtle-eval global-interp
-                                        (state (turtle 0 0 (* start-angle* 0.0174532925) (list (vector 0 0 0)) (vector .3 .1 .3))
-                                               empty
-                                               empty
-                                               empty
-                                               length*
-                                               beta*)
-                                        (eval-lsys 
-                                         (match-lambda
-                                           ['lhs '((prob (prod ...))
-                                                   ...)]
-                                           ...
-                                           [x (list (list 1 (list x)))])
-                                         generations* '(axiom* ...))))]))
+(define-syntax (kinda-quote stx ...)
+  (syntax-parse
+   stx
+   [(_ x:id) #'x]))
 
+(define-syntax (l-system stx)
+  (syntax-parse 
+   stx
+   [(l-system 
+     (~datum beta) (~datum ->) beta*:number
+     (~datum start-angle) (~datum ->) start-angle*:number 
+     (~datum length) (~datum ->) length*:number
+     (~datum generations) (~datum ->) generations*:number
+     (~datum axiom) (~datum ->) (~seq axiom*:id (~peek-not (~datum ->))) ...
+     (~seq lhs (~datum ->) (~seq prob:number (~datum =>) 
+                                 (~seq prod:id (~peek-not (~datum ->)))
+                                 ...)
+           ...)
+     ...)
+    #'(render (turtle-eval global-interp
+                           (state (turtle 0 0 (* start-angle* 0.0174532925) (list (vector 0 0 0)) (vector .3 .1 .3))
+                                  empty
+                                  empty
+                                  empty
+                                  length*
+                                  beta*)
+                           (eval-lsys 
+                            (match-lambda
+                              ['lhs '((prob ((kinda-quote prod ...)))
+                                      ...)]
+                              ...
+                              [x (list (list 1 (list x)))])
+                            generations* '(axiom* ...))))]))
+    
+    
+    
