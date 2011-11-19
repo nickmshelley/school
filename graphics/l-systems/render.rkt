c2003f794af8bf34b29f0fc55e39d5d4caec5cdc
Nick Shelley <nickmshelley@gmail.com>
2011-11-18 19:48:10 -0700

    Now centers on scaled image when rendered

--- graphics/l-systems/render.rkt
+++ graphics/l-systems/render.rkt
@@ -20,7 +20,7 @@
 (define (resize w h)
   (gl-matrix-mode 'projection)
   (gl-load-identity)
-  (gluPerspective 45 1 1 100)
+  (gluPerspective 45 1 .1 100)
   (gl-matrix-mode 'modelview)
   (gl-viewport 0 0 w h)
   #t)
@@ -32,13 +32,41 @@
     (gl-vertex-v (vector->gl-float-vector vert)))
   (gl-end))
 
+(define (analyze lines)
+  (define xs 
+    (append-map (lambda (v)
+                  (map vector-ref v
+                       (make-list (length v) 0))) 
+                lines))
+  (define ys
+    (append-map (lambda (v)
+                  (map vector-ref v
+                       (make-list (length v) 1))) 
+                lines))
+  (define zs
+    (append-map (lambda (v)
+                  (map vector-ref v
+                       (make-list (length v) 2))) 
+                lines))
+  (define x-ave (/ (+ (apply min xs)
+                      (apply max xs))
+                   2))
+  (define y-ave (/ (+ (apply min ys)
+                      (apply max ys))
+                   2))
+  (define z-ave (/ (+ (apply min zs)
+                      (apply max zs))
+                   2))
+  (define z-pos (max (- (apply max xs) (apply min xs))
+                     (- (apply max ys) (apply min ys))))
+  (values x-ave y-ave z-ave (max (* z-pos 1.3) 1)))
 
-(define (draw-opengl lines colors)
+(define (draw-opengl lines colors x-ave y-ave z-ave z-pos)
   (gl-clear 'color-buffer-bit 'depth-buffer-bit)
   (gl-push-matrix)
   (gl-line-width 2)
-  (gluLookAt 0 3 10
-             0 3 0
+  (gluLookAt x-ave y-ave z-pos
+             x-ave y-ave z-ave
              0 1 0)
   (map draw-line lines colors)
   (gl-pop-matrix))
@@ -50,6 +78,9 @@
     (init-field lines)
     (init-field colors)
     
+    (define-values (x-ave y-ave z-ave z-pos)
+      (analyze lines))
+    
     (define/public (gl-init)
       (with-gl-context
        (lambda ()
@@ -58,7 +89,7 @@
     (define/override (on-paint)
       (with-gl-context
        (lambda ()
-         (draw-opengl lines colors)
+         (draw-opengl lines colors x-ave y-ave z-ave z-pos)
          (swap-gl-buffers)
          (queue-callback (lambda x (send this refresh)) #f))))
     
@@ -74,8 +105,8 @@
                           (state-branch-verts final-state)))
   (define the-colors (cons (turtle-color (state-turt final-state))
                            (state-colors final-state)))
-
-  (define win (new frame% (label "OpenGl Test")))
+  
+  (define win (new frame% (label "L-Systems")))
   (define gl (new my-canvas% (parent win) (min-width 800) (min-height 800) (lines the-lines) (colors the-colors)))
   (send gl gl-init)
   (send win show #t))
