1ca31eab4b7490cc21e4e201b0d138f636bfe700
Nick Shelley <nickmshelley@gmail.com>
2011-12-07 13:31:54 -0700

    generates passwd and shadow files from file input

--- security/password-crack/password-crack.rkt
+++ security/password-crack/password-crack.rkt
@@ -5,6 +5,26 @@
 
 ;(crypt "hellohi" "$6$F")
 
+(define (write-user-line user)
+  (define to-write
+    (string-append user ":x:::::"))
+  (write-string to-write passwd-out)
+  (newline passwd-out))
+
+(define (write-shadow-line user password)
+  (define to-write
+    (string-append user ":" (crypt password "$1$F") ":::::::"))
+  (write-string to-write shadow-out)
+  (newline shadow-out))
+
 (define in (open-input-file "users.txt"))
+(define passwd-out (open-output-file "passwd" #:exists 'replace))
+(define shadow-out (open-output-file "shadow" #:exists 'replace))
+
 (for ([line (in-port read-line in)])
-  (printf "~v~n" line))
+  (define splitted (regexp-split " " line))
+  (write-user-line (first splitted))
+  (write-shadow-line (first splitted) (second splitted)))
+
+(close-output-port passwd-out)
+(close-output-port shadow-out)
