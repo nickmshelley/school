814d596968284022c2717b63c36c174a7956c983
Nick Shelley <nickmshelley@gmail.com>
2011-12-08 15:01:05 -0700

    minor changes to some models

--- graphics/l-systems/3d-hilbert.rkt
+++ graphics/l-systems/3d-hilbert.rkt
@@ -3,7 +3,7 @@
 beta -> 90
 start-heading -> (0 0 -1)
 start-up -> (0 1 0)
-radius -> .03
+radius -> .02
 length -> .1
 generations -> 3
 axiom -> A
--- graphics/l-systems/bush.rkt
+++ graphics/l-systems/bush.rkt
@@ -3,8 +3,8 @@
 beta -> 22.5
 start-heading -> (0 1 0)
 start-up -> (0 0 1)
-radius -> .01
+radius -> .02
 length -> .1
-generations -> 4
-axiom -> F - F
+generations -> 5
+axiom -> F
 F -> 1 => F F - [- F + F + F] + [+ F - F - F]
